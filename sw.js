a4bd5d3cf49578b62820598225b233c9bcabb7b3
gourdinstephane-oss <gourdin.stephane@gmail.com>
2026-07-23 17:44:31 +0200

    sw.js

--- sw.js
+++ sw.js
@@ -1,16 +1,10 @@
-const CACHE_NAME = 'tirelire-v4';
+const CACHE_NAME = 'tirelire-v4'; //[span_3](start_span)[span_3](end_span)
 
-self.addEventListener('install', (e) => {
-  self.skipWaiting();
-});
+self.oninstall = () => self.skipWaiting(); //[span_4](start_span)[span_4](end_span)
 
-self.addEventListener('activate', (e) => {
-  e.waitUntil(
-    caches.keys().then((keys) => Promise.all(keys.map((k) => caches.delete(k))))
-  );
-  self.clients.claim();
-});
+self.onactivate = (e) => {
+  e.waitUntil(caches.keys().then(keys => Promise.all(keys.map(k => caches.delete(k))))); //[span_5](start_span)[span_5](end_span)
+  self.clients.claim(); //[span_6](start_span)[span_6](end_span)
+};
 
-self.addEventListener('fetch', (e) => {
-  e.respondWith(fetch(e.request).catch(() => caches.match(e.request)));
-});
+self.onfetch = (e) => e.respondWith(fetch(e.request).catch(() => caches.match(e.request))); //[span_7](start_span)[span_7](end_span)
